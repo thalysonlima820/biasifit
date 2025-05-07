a728767d36440f94acddb0a8c97da1f4813e79d5
thalyson <thalysonlima820@gmail.com>
2025-05-07 11:33:38 -0300

    h

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import img1 from './assets/1_amarelo_.png'
+import img2 from './assets/2_amarelo_.png'
+import mulher from './assets/mulher.png'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <img className='amarelo1' src="../public/1_amarelo_.png" alt="" />
+      <img className='amarelo1' src={img1} alt="" />
 
       <div className='textAvalia'>
         <h1 className='faca'>FAÇA A SUA</h1>
         <h1 className='avaliacao'>AVALIAÇÃO</h1>
         <button className='btn'><span className='btnTxt'>CLIQUE AQUI</span></button>
       </div>
-      <img className='amarelo2' src="../public/2_amarelo_.png" alt="" />
+      <img className='amarelo2' src={img2} alt="" />
 
-      <img className='mulher' src="../public/mulher.png" alt="" />
+      <img className='mulher' src={mulher} alt="" />
     </>
   )
 }
