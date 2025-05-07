13142316481c41a362876bbed9519617ad5dcf59
thalyson <thalysonlima820@gmail.com>
2025-05-07 11:23:58 -0300

    jk

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img className='amarelo1' src="../public/1_amarelo_.png" alt="" />
+
+      <div className='textAvalia'>
+        <h1 className='faca'>FAÇA A SUA</h1>
+        <h1 className='avaliacao'>AVALIAÇÃO</h1>
+        <button className='btn'><span className='btnTxt'>CLIQUE AQUI</span></button>
+      </div>
+      <img className='amarelo2' src="../public/2_amarelo_.png" alt="" />
+
+      <img className='mulher' src="../public/Imagem_.png" alt="" />
+    </>
+  )
+}
+
+export default App
