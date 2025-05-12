d169d39e260b6c08b625cf289fd871ce6ecf3608
thalysonlima820 <thalysonlima820@gmail.com>
2025-05-12 10:38:19 -0300

    bi

--- src/page/Plano.tsx
+++ src/page/Plano.tsx
@@ -11,18 +11,6 @@ const Plano = () => {
       <h1 className='title'>NOSSOS PLANOS</h1>
 
       <div className='boxMaster'>
-        <div className='box'>
-          <h1 className='texth1'>SEMESTRAL</h1>
-          <h1 className='textValor'><span>R$ 130</span>,00</h1>
-          <p className='textmes'>por mês</p>
-
-          <a href="https://evo-totem.w12app.com.br/biasifit/1/site/QhXXzoY7OMy%5BPLUS%5DFpULG15Wrw%5BEQUAL%5D%5BEQUAL%5D" target="_blank" rel="noopener noreferrer">
-            <button className='btnContratar'>CONTRATAR AGORA</button>
-          </a>
-
-          <h4 className='textmensalidade'>R$ 779,99 em até 6x</h4>
-          <h3 className='textDuracao'>Duração de 6 meses</h3>
-        </div>
 
         <div className='box'>
           <h1 className='texth1'>TRIMESTRAL</h1>
@@ -34,6 +22,23 @@ const Plano = () => {
           </a>
           <h4 className='textmensalidade'>R$ 500,00 em até 3x</h4>
           <h3 className='textDuracao'>Duração de 3 meses</h3>
+        </div>
+
+        <div className='box'>
+          <div className='boxVanta'>
+            <h1 className='textVantajoso'>MAIS VANTAJOSO</h1>
+          </div>
+          <h1 className='texth1'>SEMESTRAL</h1>
+          <h1 className='textValor'><span>R$ 130</span>,00</h1>
+          <p className='textmes'>por mês</p>
+
+          <a href="https://evo-totem.w12app.com.br/biasifit/1/site/QhXXzoY7OMy%5BPLUS%5DFpULG15Wrw%5BEQUAL%5D%5BEQUAL%5D" target="_blank" rel="noopener noreferrer">
+            <button className='btnContratar'>CONTRATAR AGORA</button>
+          </a>
+
+          <h4 className='textmensalidade'>R$ 779,99 em até 6x</h4>
+          <h3 className='textDuracao'>Duração de 6 meses</h3>
+
         </div>
 
         <div className='box'>
